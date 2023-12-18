5777eb318975959c9aa3260d44b2824a48767b6b
Andrii Hromanchuk <73118147+TrueMage@users.noreply.github.com>
2023-12-18 02:15:38 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_BlurImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MakeBlurry(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            BlurEffect blur = new BlurEffect();
+            blur.Radius= e.NewValue;
+            photo.Effect = blur;
+        }
+    }
+}
